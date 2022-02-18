a22989fbbf43b7dd6daca2fb811eb917cbd8980b
刘志华 <liuzhihua@shangjietech.com>
2022-02-18 13:45:34 +0800

    初始化项目

---
+++ js/request.js
@@ -0,0 +1,50 @@
+//axios拦截器
+const service = axios.create({
+  baseURL: 'http://rap2api.taobao.org/app/mock/289298',
+  timeout: 30000,
+});
+
+service.interceptors.request.use(
+  (config) => {
+    if (config.method == 'get') {
+      config.params = config.data
+      delete config.data
+    }
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+service.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+    if (res.code != 200) {
+      el.$message({
+        title: '提示',
+        message: res.msg || 'Error',
+        type: 'error',
+        duration: 5 * 1000,
+      });
+      return Promise.reject(new Error(res.msg || 'Error'));
+    } else {
+      if (res.msg) {
+        el.$message({
+          message: res.msg,
+          type: 'success'
+        })
+      }
+      return res;
+    }
+  },
+  (error) => {
+    console.log('err' + error);
+    el.$message({
+      message: error.message,
+      type: 'error',
+      duration: 5 * 1000,
+    });
+    return Promise.reject(error);
+  }
+);
