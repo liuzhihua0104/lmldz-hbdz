70e82f1f4bf3cf99d47a1b00dd7ad813d68fc786
刘志华 <liuzhihua@shangjietech.com>
2022-02-23 10:05:37 +0800

    新增测试文件

---
+++ vue1.js
@@ -0,0 +1,101 @@
+function getUrlQuery() {
+    let search = location.search
+    let obj = {}
+    if (search) {
+      let query = search.substring(1).split('&')
+      query.forEach((queryItem) => {
+        let keyValue = queryItem.split('=')
+        let [key, value] = keyValue
+        if (key) {
+          obj[key] = value
+        }
+      })
+    }
+    return obj
+  }
+
+
+   let Vm=new Vue({
+    el: '#app',
+    data() {
+      return {
+        formData: {
+          user: '123',
+          remark: '',
+          radio1: ""
+        },
+        formRules: {
+          user: [
+            { required: true, message: '请输入定向名称', trigger: 'change' },
+          ],
+          radio1: [
+            { required: true, message: '请选择', trigger: 'change' },
+          ],
+
+        },
+
+      }
+    },
+    watch: {
+
+    },
+    methods: {
+      // 返回
+      onCancel() {
+        window.history.go(-1)
+      },
+      onConfirm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            this.getStrategyAddEdit()
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      },
+      // 新增/编辑保存表单
+      getStrategyAddEdit() {
+        service({
+          url: '/user/strategy/inversion/create',
+          method: 'post',
+          data: this.formData,
+        }).then(({ data }) => { })
+      },
+
+      // 编辑获取表单详情
+      getDetails() {
+        service({
+          url: '/user/strategy/inversion/detail',
+          method: 'get',
+          data: {
+            id: this.formData.id,
+          },
+        }).then(({ data }) => {
+          Object.keys(this.formData).forEach((key) => {
+            console.log(key, data[key])
+            this.formData[key] = data[key]
+          })
+        })
+      },
+    },
+    created() {
+      let query = getUrlQuery()
+      if (query.id) {
+        this.formData.id = query.id
+        this.getDetails()
+      }
+      // 获取项目列表
+      // this.getProjectList()
+    },
+  })
+
+
+
+//   module.exports={
+//    Vm:Vm
+// }
+
+module.exports={
+    Vm:Vm,
+}
