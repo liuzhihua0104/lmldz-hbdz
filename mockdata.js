851365ba097ec4322a8960f9bd389ef6cb042025
刘志华 <liuzhihua@shangjietech.com>
2022-03-08 18:49:50 +0800

    素材源奖品和定向接口添加

--- mockdata.js
+++ mockdata.js
@@ -53,7 +53,7 @@ let doPlanList = {
                 "prizeName": null,
                 "strategyPrize": null,
                 "strategyName": null,
-                "prizeCount": 3
+                "prizeCount": null
             }
         ]
     }
@@ -82,128 +82,131 @@ let doPlanApplyList = {
 let sList = {
     "code": 200,
     "msg": null,
-    "data": [
-        {
-            "id": "87ae8d1f62ec4eb097c42f0c6c085881",
-            "isTrue": 1,
-            "interType": null,
-            "name": "北京青啤经典青岛啤酒白啤",
-            "brandName": "北京青啤经典",
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1631931141000,
-            "endTime": null
-        },
-        {
-            "id": "5a2d95ce43544402a683f300d5a74922",
-            "isTrue": 1,
-            "interType": null,
-            "name": "北京青啤经典东奥黄金冰墩墩吊坠",
-            "brandName": "北京青啤经典",
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1631930937000,
-            "endTime": null
-        },
-        {
-            "id": "34fcdf0fac6e42bea4223ba359b694c5",
-            "isTrue": 1,
-            "interType": null,
-            "name": "26g千足金纪念金牌",
-            "brandName": "红牛",
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1630396180000,
-            "endTime": null
-        },
-        {
-            "id": "3c7d617d39064a6891860d88e1635dc9",
-            "isTrue": 1,
-            "interType": null,
-            "name": "一元换购红金瓶",
-            "brandName": null,
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1630310107000,
-            "endTime": null
-        },
-        {
-            "id": "f468520d61b148949028e88b440a0888",
-            "isTrue": 1,
-            "interType": null,
-            "name": "HB红包",
-            "brandName": "虚拟红包",
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1629705203000,
-            "endTime": null
-        },
-        {
-            "id": "3cb5cf2db4c14d98accce4362d28bf86",
-            "isTrue": 1,
-            "interType": null,
-            "name": "抢红包名称",
-            "brandName": "虚拟",
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1629704530000,
-            "endTime": null
-        },
-        {
-            "id": "a10830be520c4dd49cfa1e1c9d977ce9",
-            "isTrue": 1,
-            "interType": null,
-            "name": "红包",
-            "brandName": "虚拟",
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1629704287000,
-            "endTime": null
-        },
-        {
-            "id": "64b460c76e92435eb5d525adacc82358",
-            "isTrue": 1,
-            "interType": null,
-            "name": "这是一条测试数据",
-            "brandName": "辉山",
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1629704209000,
-            "endTime": null
-        },
-        {
-            "id": "b38174e986e8406faf4caa9fda70e6eb",
-            "isTrue": 1,
-            "interType": null,
-            "name": "现金红包0.88",
-            "brandName": null,
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1629699929000,
-            "endTime": null
-        },
-        {
-            "id": "a8aed8641b134a3cbb3c4220e5479980",
-            "isTrue": 1,
-            "interType": null,
-            "name": "辉山箱奶华4.88元",
-            "brandName": "辉山",
-            "organType": null,
-            "pageType": null,
-            "createUser": null,
-            "createTime": 1629690290000,
-            "endTime": null
-        }
-    ]
+    "data": {
+        "total": 1101,
+        "list": [
+            {
+                "id": "87ae8d1f62ec4eb097c42f0c6c085881",
+                "isTrue": 1,
+                "interType": null,
+                "name": "北京青啤经典青岛啤酒白啤",
+                "brandName": "北京青啤经典",
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1631931141000,
+                "endTime": null
+            },
+            {
+                "id": "5a2d95ce43544402a683f300d5a74922",
+                "isTrue": 1,
+                "interType": null,
+                "name": "北京青啤经典东奥黄金冰墩墩吊坠",
+                "brandName": "北京青啤经典",
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1631930937000,
+                "endTime": null
+            },
+            {
+                "id": "34fcdf0fac6e42bea4223ba359b694c5",
+                "isTrue": 1,
+                "interType": null,
+                "name": "26g千足金纪念金牌",
+                "brandName": "红牛",
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1630396180000,
+                "endTime": null
+            },
+            {
+                "id": "3c7d617d39064a6891860d88e1635dc9",
+                "isTrue": 1,
+                "interType": null,
+                "name": "一元换购红金瓶",
+                "brandName": null,
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1630310107000,
+                "endTime": null
+            },
+            {
+                "id": "f468520d61b148949028e88b440a0888",
+                "isTrue": 1,
+                "interType": null,
+                "name": "HB红包",
+                "brandName": "虚拟红包",
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1629705203000,
+                "endTime": null
+            },
+            {
+                "id": "3cb5cf2db4c14d98accce4362d28bf86",
+                "isTrue": 1,
+                "interType": null,
+                "name": "抢红包名称",
+                "brandName": "虚拟",
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1629704530000,
+                "endTime": null
+            },
+            {
+                "id": "a10830be520c4dd49cfa1e1c9d977ce9",
+                "isTrue": 1,
+                "interType": null,
+                "name": "红包",
+                "brandName": "虚拟",
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1629704287000,
+                "endTime": null
+            },
+            {
+                "id": "64b460c76e92435eb5d525adacc82358",
+                "isTrue": 1,
+                "interType": null,
+                "name": "这是一条测试数据",
+                "brandName": "辉山",
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1629704209000,
+                "endTime": null
+            },
+            {
+                "id": "b38174e986e8406faf4caa9fda70e6eb",
+                "isTrue": 1,
+                "interType": null,
+                "name": "现金红包0.88",
+                "brandName": null,
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1629699929000,
+                "endTime": null
+            },
+            {
+                "id": "a8aed8641b134a3cbb3c4220e5479980",
+                "isTrue": 1,
+                "interType": null,
+                "name": "辉山箱奶华4.88元",
+                "brandName": "辉山",
+                "organType": null,
+                "pageType": null,
+                "createUser": null,
+                "createTime": 1629690290000,
+                "endTime": null
+            }
+        ]
+    }
 }
 
 
