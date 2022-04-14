a00981fa5a62f2c1d3a2d358f6cbfa4d010fd0e1
刘志华 <liuzhihua@shangjietech.com>
2022-04-14 17:47:47 +0800

    调整

--- areaDemo/my-component.vue
+++ areaDemo/my-component.vue
@@ -1,9 +1,8 @@
 <template>
 
-  <div style="padding:20px">
-    <a-tree-select tree-node-filter-prop="title" :allow-clear="true" v-model="value" style="width: 100%" :tree-data="treeData" tree-checkable :show-checked-strategy="SHOW_PARENT" search-placeholder="请选择" />
+  <div style="padding:20px;width:1000px">
+    <a-tree-select style="width:100%;max-height:100px;overflow-y:scroll;border:1px solid grey" dropdown-class-name="dropdownClassName" tree-node-filter-prop="title" :allow-clear="true" v-model="value" style="width: 100%" :tree-data="treeData" tree-checkable :show-checked-strategy="SHOW_PARENT" search-placeholder="请选择" />
 
-    <a-cascader v-model="cascValue" style="width: 100%" multiple max-tag-count="responsive" :options="options" placeholder="请选择"></a-cascader>
   </div>
 </template>
  
@@ -33,25 +32,6 @@ const SHOW_PARENT = TreeSelect.SHOW_PARENT;
 let treeData = parseJson(areaOptions)
 
 
-// ==========以下是级联
-function cascJson(arr) {
-  arr = arr.slice()
-  function toParse(arr) {
-    arr.forEach(function (item) {
-      // item.key = item.value;
-      // item.title = item.label;
-      if (item.children && item.children.length > 0 && Array.isArray(item.children)) {
-        toParse(item["children"])
-      } else {
-        delete item.children
-      }
-    })
-    return arr
-  }
-  return toParse(arr)
-}
-
-
 
 module.exports = {
   data: function () {
@@ -59,11 +39,6 @@ module.exports = {
       treeData: treeData,
       value: [11],
       SHOW_PARENT,
-
-
-      // 以下是级联
-      options: cascJson(areaOptions),
-      cascValue: []
 
     }
   },
@@ -74,4 +49,8 @@ module.exports = {
 </script>
  
 <style>
+.dropdownClassName {
+  height: 200px;
+  overflow-y: scroll;
+}
 </style>
