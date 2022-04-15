f172adb16a42d931190050528c8052cac73dc45e
刘志华 <liuzhihua@shangjietech.com>
2022-04-15 15:05:13 +0800

    修改配置

--- areaDemo/my-component.vue
+++ areaDemo/my-component.vue
@@ -1,49 +1,32 @@
 <template>
 
   <div style="padding:20px;width:1000px">
-    <a-tree-select  dropdown-class-name="dropdownClassName" tree-node-filter-prop="title" :allow-clear="true" v-model="value" style="width: 100%" :tree-data="treeData" tree-checkable :show-checked-strategy="SHOW_PARENT" search-placeholder="请选择" />
+    <!-- field-names="{children:'children', title:'label', key:'value' }"     -->
+    <a-tree-select
+
+    replace-fields="{children:'children', label:'name', key:'code', value: 'code' }" 
+      dropdown-class-name="dropdownClassName" tree-node-filter-prop="title" :allow-clear="true" v-model="value" style="width: 100%" :tree-data="treeData" tree-checkable :show-checked-strategy="SHOW_PARENT" search-placeholder="请选择" />
 
   </div>
 </template>
  
 <script>
 
-// 递归处理城市数据
-function parseJson(arr) {
-  arr = arr.slice()
-  function toParse(arr) {
-    arr.forEach(function (item) {
-      item.key = item.value;
-      item.title = item.label;
-      if (item.children && item.children.length > 0 && Array.isArray(item.children)) {
-        toParse(item["children"])
-      } else {
-        delete item.children
-      }
-    })
-    return arr
-  }
-  return toParse(arr)
-}
-
 let TreeSelect = antd.TreeSelect
 const SHOW_PARENT = TreeSelect.SHOW_PARENT;
-
-let treeData = parseJson(areaOptions)
-
 
 
 module.exports = {
   data: function () {
     return {
-      treeData: treeData,
+      treeData: areaOptions,
       value: [],
       SHOW_PARENT,
 
     }
   },
   mounted() {
-    // console.log(this.treeData)
+
   }
 }
 </script>
